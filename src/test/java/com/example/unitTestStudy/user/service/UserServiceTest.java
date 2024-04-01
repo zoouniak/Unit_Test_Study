3c53d5dcb7db8f4a43986f4150df50429adaac54
adorableco <tpdusdl1001@knu.ac.kr>
2024-04-01 11:08:53 +0900

    Fix: UserService
    - `@SpringBootTest` 제거
    - 단위테스트로 변경

--- src/test/java/com/example/unitTestStudy/user/service/UserServiceTest.java
+++ src/test/java/com/example/unitTestStudy/user/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.unitTestStudy.user.service;
 
+import com.example.unitTestStudy.user.converter.UserConverter;
 import com.example.unitTestStudy.user.domain.User;
 import com.example.unitTestStudy.user.dto.UserCreateRequestDto;
 import com.example.unitTestStudy.user.dto.UserResponseDto;
@@ -7,20 +8,29 @@ import com.example.unitTestStudy.user.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
-    @Autowired
+    @InjectMocks
     private UserService sut;
 
-    @Autowired
+    @Mock
     private UserRepository userRepository;
+
+    @Spy
+    private UserConverter userConverter;
 
 
     @Test
@@ -32,26 +42,33 @@ class UserServiceTest {
                 .age(user.getAge())
                 .hobby(user.getHobby())
                 .build();
+        Mockito.when(userRepository.save(userConverter.toUser(requestDto))).thenReturn(user);
 
-        Long savedId = sut.save(requestDto);
+        Long save = sut.save(requestDto);
 
-        assertThat(savedId).isEqualTo(1);
-        Assertions.assertThat(user.getName()).isEqualTo(userRepository.findById(savedId).get().getName());
+        Assertions.assertThat(save).isEqualTo(userConverter.toUserResponseDto(user).getId());
+
+
     }
 
     @Test
     @DisplayName("id로 멤버 조회에 성공한다.")
     void Find_member_by_id() {
+        User savedUser = createUserRequest();
+        Mockito.when(userRepository.findById(savedUser.getId())).thenReturn(Optional.of(savedUser));
 
+        UserResponseDto findUser = sut.findOne(savedUser.getId());
+
+        assertThat(findUser.getId()).isEqualTo(savedUser.getId());
+    }
+
+    // 팩토리 메서드로 테스트 픽스처 생성
+     private User createUserRequest(){
         UserCreateRequestDto requestDto = UserCreateRequestDto.builder()
-                .name("member1")
-                .age(10)
+                .name("Seyeon")
+                .age(20)
+                .hobby("coding")
                 .build();
-        Long savedId = sut.save(requestDto);
-
-        UserResponseDto responseDto = sut.findOne(savedId);
-
-        assertThat(responseDto.getName()).isEqualTo(requestDto.getName());
-        System.out.println("responseDto.getName() = " + responseDto.getName() + " requestDto.getName() = "+requestDto.getName());
+        return userConverter.toUser(requestDto);
     }
 }
